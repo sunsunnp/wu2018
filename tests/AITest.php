d593662afc027e9045ed3427ce2f302d97213c22
sunsunnp <sunsun.np@gmail.com>
2018-02-03 17:08:54 +0700

    sunsun

--- tests/AITest.php
+++ tests/AITest.php
@@ -10,4 +10,12 @@ final class AITest extends TestCase
         $expected_result = 'Male';
         $this->assertEquals($expected_result, $result);
     }
+
+    public function testGender_Female(): void
+    {
+        $result = AI::getGender('สวัสดีค่ะ');
+        $expected_result = 'Female';
+        $this->assertEquals($expected_result, $result);
+    }
+
 }
