7083dda66ecd703326f6915245d9e996f3489bbf
sunsunnp <sunsun.np@gmail.com>
2018-02-04 10:26:35 +0700

    sunsun

--- app/ai.php
+++ app/ai.php
@@ -68,10 +68,13 @@ class AI
         if (!empty($matches)) {
             array_push($result, 'TH');
         }
-        else {
+        
+        $en = '/[a-zA-Z]+/u';
+        preg_match_all($en, $text, $matches, PREG_SET_ORDER, 0);
+        if (!empty($matches)) {
             array_push($result, 'EN');
         }
-        
+       
         return $result;
        // return ['TH', 'EN'];
     }
